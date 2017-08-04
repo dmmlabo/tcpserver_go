089f69bd665fda6b6c3f48e4a8d2c1614079f50f
Shintaro Kawamura <kawamura-shin@dmm.com>
2017-08-04 15:33:17 +0900

    Copy from tcp3 to tcp4

---
+++ tcp4/server/conn.go
@@ -0,0 +1,65 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net"
+)
+
+type Conn struct {
+	svr     *Server
+	conn    *net.TCPConn
+	readCtx context.Context
+	errRead context.CancelFunc
+}
+
+func newConn(svr *Server, tcpConn *net.TCPConn) *Conn {
+	readCtx, errRead := context.WithCancel(context.Background())
+	return &Conn{
+		svr:     svr,
+		conn:    tcpConn,
+		readCtx: readCtx,
+		errRead: errRead,
+	}
+}
+
+func (c *Conn) handleConnection() {
+	defer func() {
+		c.conn.Close()
+		c.svr.Wg.Done()
+	}()
+
+	go c.handleRead()
+
+	select {
+	case <-c.readCtx.Done():
+	case <-c.svr.ctx.Done():
+	case <-c.svr.AcceptCtx.Done():
+	}
+}
+
+func (c *Conn) handleRead() {
+	defer c.errRead()
+
+	buf := make([]byte, 4*1024)
+
+	for {
+		n, err := c.conn.Read(buf)
+		if err != nil {
+			if ne, ok := err.(net.Error); ok {
+				switch {
+				case ne.Temporary():
+					continue
+				}
+			}
+			log.Println("Read", err)
+			return
+		}
+
+		n, err = c.conn.Write(buf[:n])
+		if err != nil {
+			log.Println("Write", err)
+			return
+		}
+	}
+}
