0c50de3fcd025b327ed9f07bf3de1e0b7afef38d
Shintaro Kawamura <kawamura-shin@dmm.com>
2017-08-04 18:44:20 +0900

    Fix Write after shutdown

--- tcp4/server/conn.go
+++ tcp4/server/conn.go
@@ -7,24 +7,33 @@ import (
 )
 
 type Conn struct {
-	svr      *Server
-	conn     *net.TCPConn
-	readCtx  context.Context
-	stopRead context.CancelFunc
+	svr       *Server
+	conn      *net.TCPConn
+	ctxRead   context.Context
+	stopRead  context.CancelFunc
+	ctxWrite  context.Context
+	stopWrite context.CancelFunc
+	sem       chan struct{}
 }
 
 func newConn(svr *Server, tcpConn *net.TCPConn) *Conn {
-	readCtx, stopRead := context.WithCancel(context.Background())
+	ctxRead, stopRead := context.WithCancel(context.Background())
+	ctxWrite, stopWrite := context.WithCancel(context.Background())
+	sem := make(chan struct{}, 1)
 	return &Conn{
-		svr:      svr,
-		conn:     tcpConn,
-		readCtx:  readCtx,
-		stopRead: stopRead,
+		svr:       svr,
+		conn:      tcpConn,
+		ctxRead:   ctxRead,
+		stopRead:  stopRead,
+		ctxWrite:  ctxWrite,
+		stopWrite: stopWrite,
+		sem:       sem,
 	}
 }
 
 func (c *Conn) handleConnection() {
 	defer func() {
+		c.stopWrite()
 		c.conn.Close()
 		c.svr.Wg.Done()
 	}()
@@ -32,7 +41,7 @@ func (c *Conn) handleConnection() {
 	go c.handleRead()
 
 	select {
-	case <-c.readCtx.Done():
+	case <-c.ctxRead.Done():
 	case <-c.svr.ctx.Done():
 	case <-c.svr.AcceptCtx.Done():
 	}
@@ -66,19 +75,25 @@ func (c *Conn) handleEcho(buf []byte) {
 	// do something
 
 	// write
-	for {
-		n, err := c.conn.Write(buf)
-		if err != nil {
-			if nerr, ok := err.(net.Error); ok {
-				if nerr.Temporary() {
-					buf = buf[n:]
-					continue
-				}
-			}
-			log.Println("Write error", err)
-			// write error
-			c.stopRead()
-		}
+	select {
+	case <-c.ctxWrite.Done():
 		return
+	case c.sem <- struct{}{}:
+		defer func() { <-c.sem }()
+		for {
+			n, err := c.conn.Write(buf)
+			if err != nil {
+				if nerr, ok := err.(net.Error); ok {
+					if nerr.Temporary() {
+						buf = buf[n:]
+						continue
+					}
+				}
+				log.Println("Write error", err)
+				// write error
+				c.stopRead()
+			}
+			return
+		}
 	}
 }
