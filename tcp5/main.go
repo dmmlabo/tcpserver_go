a740e61331d63147fd5ed0b0a29207facdaf0165
Shintaro Kawamura <kawamura-shin@dmm.com>
2017-08-09 18:03:25 +0900

    Fix Restart

--- tcp5/main.go
+++ tcp5/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 
 	"github.com/dmmlabo/tcpserver_go/tcp5/server"
@@ -16,57 +17,83 @@ func main() {
 	signal.Ignore()
 	signal.Notify(chSig, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGHUP)
 
-	for {
-		restart := startup(chSig)
-		if restart {
-			continue
-		}
-		break
-	}
-}
+	var wg sync.WaitGroup
+	defer wg.Wait()
 
-func startup(chSig chan os.Signal) (restart bool) {
-	svr := server.NewServer(context.Background(), "127.0.0.1:12345")
-
+	host := loadConf()
+	svr := server.NewServer(context.Background(), host)
 	err := svr.Listen()
 
 	if err != nil {
 		log.Fatal("Listen()", err)
 	}
-
 	log.Println("Server Started")
 
-	select {
-	case sig := <-chSig:
-		switch sig {
-		case syscall.SIGINT, syscall.SIGTERM:
-			log.Println("Server Shutdown...")
-			svr.Shutdown()
+	for {
+		select {
+		case sig := <-chSig:
+			switch sig {
+			case syscall.SIGINT, syscall.SIGTERM:
+				log.Println("Server Shutdown...")
+				svr.Shutdown()
 
+				svr.Wg.Wait()
+				<-svr.ChClosed
+				log.Println("Server Shutdown Completed")
+			case syscall.SIGQUIT:
+				log.Println("Server Graceful Shutdown...")
+				svr.GracefulShutdown()
+
+				svr.Wg.Wait()
+				<-svr.ChClosed
+				log.Println("Server Graceful Shutdown Completed")
+			case syscall.SIGHUP:
+				log.Println("Server Restarting...")
+
+				oldHost := host
+				host = loadConf()
+
+				// Check host have changed
+				if host == oldHost {
+					// TODO: load config to server
+					log.Println("Server Config Loaded")
+				} else {
+					// Restart Server
+					oldSvr := svr
+					svr = server.NewServer(context.Background(), host)
+					err = svr.Listen()
+					if err != nil {
+						log.Fatal("Listen()", err)
+					}
+					log.Println("Server Restarted")
+					wg.Add(1)
+					go func() {
+						oldSvr.GracefulShutdown()
+						wg.Done()
+					}()
+				}
+				continue
+			default:
+				panic("unexpected signal has been received")
+			}
+		case <-svr.AcceptCtx.Done():
+			log.Println("Server Error Occurred")
 			svr.Wg.Wait()
 			<-svr.ChClosed
 			log.Println("Server Shutdown Completed")
-		case syscall.SIGQUIT:
-			log.Println("Server Graceful Shutdown...")
-			svr.GracefulShutdown()
-
-			svr.Wg.Wait()
-			<-svr.ChClosed
-			log.Println("Server Graceful Shutdown Completed")
-		case syscall.SIGHUP:
-			log.Println("Server Restarting...")
-			svr.GracefulShutdown()
-
-			<-svr.ChClosed
-			restart = true
-		default:
-			panic("unexpected signal has been received")
 		}
-	case <-svr.AcceptCtx.Done():
-		log.Println("Server Error Occurred")
-		svr.Wg.Wait()
-		<-svr.ChClosed
-		log.Println("Server Shutdown Completed")
+		return
 	}
-	return
+}
+
+var first = true
+
+func loadConf() string {
+	// TODO: load config from file or env
+	if first {
+		first = false
+		return "127.0.0.1:12345"
+	} else {
+		return "127.0.0.1:12346"
+	}
 }
